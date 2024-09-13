c826216688704b24605d05267d88f663862b30e7
Valentin Goxhaj <valentinogoxhaj@gmail.com>
2024-09-13 17:17:29 +0200

    nav bar animation

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
 	const header = document.querySelector('header');
 	const nav = document.querySelector('header nav');
+	const button = document.querySelector('header ul li:last-child');
 	const headerHeight = header.offsetHeight;
 	const threshold = headerHeight * 0.8;
+
+	console.log(button); // Check if the button is selected
 
 	window.addEventListener('scroll', function() {
 		if (window.scrollY > threshold) {
 			nav.classList.add('nav-scrolled');
+			button.classList.add('button-scrolled');
 		} else {
 			nav.classList.remove('nav-scrolled');
+			button.classList.remove('button-scrolled');
 		}
 	});
 });
